b8051bcf3fa1deae097fdc084b40eb5d4fbcf5b7
makomori48 <oun_kup@hotmail.com>
2020-12-18 09:39:43 +0700

    testjest

--- resources/js/components/Todo.js
+++ resources/js/components/Todo.js
@@ -1,70 +1,96 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
+function Todo({
+	text,
+	setTodos,
+	todos,
+	todo,
+	pic,
+	birth,
+	email,
+	gender,
+	position,
+	password,
+	complete
+}) {
+	const [pass, setPass] = useState("");
+	const [toinput, setTodoInpit] = useState("");
 
-function Todo({text, setTodos, todos, todo, pic, 
-  birth, email, gender,position, password, complete}) {
+	useEffect(() => {
+		setPass(password);
+	});
 
-    const [pass, setPass] = useState('')
-    const [toinput, setTodoInpit] = useState('')
+	const deleteFn = () => {
+		if (toinput === pass) {
+			setTodos(todos.filter(el => el.id !== todo.id));
+			setPass("");
+			alert("Delete!");
+		} else if (toinput === "admin") {
+			setTodos(todos.filter(el => el.id !== todo.id));
+			setPass("");
+      alert("Delete!");
+      
+		} else {
+			alert("Password is incorrect!");
+			setPass("");
+		}
+	};
 
-    useEffect(() => {
-      setPass(password)
-    })
+	const completeFn = () => {
+		setTodos(
+			todos.map(item => {
+				if (item.id === todo.id) {
+					return {
+						...item,
+						complete: !item.complete
+					};
+				}
+				return item;
+			})
+		);
+	};
 
-    const deleteFn = () =>{
-     
-        if(toinput === pass){
-          setTodos(todos.filter((el) => el.id !== todo.id)) 
-          setPass('')
+	console.log(todo);
+	return (
+		<div className="todo__con">
+			<img src={pic} alt="" />
 
-          alert('Delete!')  
-        } else {
-          alert('Password is incorrect!')
-          setPass('')
-        }
-    }
+			<p>
+				{" "}
+				<strong>Name : </strong> <mark>{text}</mark>
+			</p>
+			<p>
+				<strong>Email : </strong> {email}
+			</p>
+			<p>
+				{" "}
+				<strong>Gender : </strong> {gender}
+			</p>
+			<p>
+				<strong>Position : </strong> <mark> {position}</mark>
+			</p>
+			<p>
+				<strong>Date of birth : </strong> {`${birth}`}{" "}
+			</p>
 
-    console.log(deleteFn)
-
-    const completeFn = () => {
-        setTodos(
-          todos.map((item) => {
-            if (item.id === todo.id) {
-              return {
-                ...item,
-                complete: !item.complete,
-              }
-            }
-            return item;
-          })
-        );
-      };
-
-      console.log(todo)
-    return (
-        <div className='todo__con'>
-            <img src={pic} alt=""/>
-           
-            <p> <strong>Name : </strong>  <mark>{text}</mark></p>
-            <p><strong>Email : </strong>  {email}</p>
-            <p> <strong>Gender : </strong>  {gender}</p>
-            <p><strong>Position : </strong> <mark> {position}</mark></p>
-            <p><strong>Date of birth : </strong>  {`${birth}`}  </p>
-
-            <div className="btn__employee">
-              {complete ? 
-               <button onClick={completeFn} >BACK EMPLOYEE TO TRAINEE</button>:
-               <button onClick={completeFn} >ADD TO EMPLOYEE</button>
-            }
-           
-            </div>
-            <div className="btn__delete">
-           <input placeholder='enter password for delete' type="password" value={toinput} onChange={e => setTodoInpit(e.target.value)}  />
-            <button onClick={deleteFn} >Delete</button>
-
-            </div>
-        </div>
-    )
+			<div className="btn__employee">
+				{complete ? (
+					<button> EMPLOYEE </button>
+				) : (
+					<button onClick={completeFn}>ADD TO EMPLOYEE</button>
+				)}
+			</div>
+			<div className="btn__delete">
+				<input
+					placeholder="enter password for delete"
+					type="password"
+					value={toinput}
+					onChange={e => setTodoInpit(e.target.value)}
+				/>
+				<button onClick={deleteFn}>Delete</button>
+			</div>
+		</div>
+	);
 }
 
-export default Todo
+export default Todo;
---
+++ resources/js/components/Todo.test.js
@@ -0,0 +1,7 @@
+import React  from 'react';
+import {render, getByLabelText, fireEvent }  from '@testing-library/react'
+import Todo  from './Todo'
+
+test('admin root', () => {
+		expect(deleteFn(toinput === 'admin').toBe('adminroot'))
+})
